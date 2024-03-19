0e55c05864871b295274c4414ec3c78543d5d3c0
Emre Yavuz <emreeyavuzz98@gmail.com>
2024-03-19 22:22:06 +0300

    TC_307 and TC_308 Elements and Test Cases Finished

--- src/US_307/TC_307.java
+++ src/US_307/TC_307.java
@@ -1,4 +1,33 @@
 package US_307;
 
-public class TC_307 {
+import Utility.BaseDriver;
+import Utility.Tools;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TC_307 extends BaseDriver {
+    /*Acceptance Criteria:
+    Find the "E-Commerce By E-Junkie" link on homepage and click.
+    Click the e-junkie logo at the top left on the opened page.
+    Verify that URL is " ".*/
+    @Test
+    public void TC_307() {
+        
+        TC307_308_Elements elements = new TC307_308_Elements();
+
+        UrlOpen();
+
+        elements.EcommerceByJunkieLink.click();
+
+        Tools.Bekle(1);
+
+        elements.EcommerceByJunkieLogo.click();
+
+        Tools.Bekle(2);
+
+        System.out.println(driver.getCurrentUrl());
+
+        Assert.assertTrue(driver.getCurrentUrl().equals("https://www.e-junkie.com/"), "Test failed");
+    }
 }
