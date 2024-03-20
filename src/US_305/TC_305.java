cb4649b48e3b90c94abd4e2e01ac29b9726fb7c2
ozgurrkarakus <ozgurkarakus.fb@gmail.com>
2024-03-20 23:44:48 +0300

    Demo

--- src/US_305/TC_305.java
+++ src/US_305/TC_305.java
@@ -1,4 +1,53 @@
 package US_305;
 
-public class TC_305 {
+import Utility.BaseDriver;
+import Utility.Tools;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_305 extends BaseDriver {
+    @Test
+    public void Tc_305() {
+        TC305_306_Elements elements = new TC305_306_Elements();
+        UrlOpen();
+
+        elements.addcart.click();
+
+        Tools.Bekle(3);
+        driver.switchTo().frame(elements.iframeodeme);
+        Tools.Bekle(1);
+        elements.creditCard.click();
+        Tools.Bekle(3);
+
+
+        elements.mail.sendKeys("gencer@gmail.com");
+        elements.confirmail.sendKeys("gencer@gmail.com");
+        elements.nameOncard.sendKeys("gencer genc");
+
+
+        driver.switchTo().frame(2);
+
+        elements.credtCardInput.sendKeys("4242 4242 4242 4242");
+        elements.experidationDay.sendKeys("1224");
+        elements.ccv.sendKeys("000");
+
+        driver.switchTo().parentFrame();
+
+        elements.payclick.click();
+        wait.until(ExpectedConditions.urlContains("gencer"));
+        Tools.Bekle(3);
+        elements.downlandbtn.click();
+
+
+        Assert.assertTrue(elements.downlandbtn.isDisplayed());
+        if (elements.downlandbtn.isDisplayed()) {
+            System.out.println("TEST PASSED");
+        } else System.out.println("TEST FAIL");
+
+
+        driver.quit();
+
+    }
 }
+
--- src/US_306/TC_306.java
+++ src/US_306/TC_306.java
@@ -1,4 +1,24 @@
 package US_306;
 
-public class TC_306 {
+import US_305.TC305_306_Elements;
+import Utility.BaseDriver;
+import Utility.Tools;
+import org.testng.annotations.Test;
+
+public class TC_306 extends BaseDriver {
+    @Test
+    public void Tc_306() {
+        TC305_306_Elements cm = new TC305_306_Elements();
+        UrlOpen();
+        cm.contact.click();
+        Tools.Bekle(3);
+        cm.name.sendKeys("Özgür");
+        cm.email.sendKeys("Ozgur@gmail.com");
+        cm.subject.sendKeys("Test101");
+        cm.message.sendKeys("The message area is here. ");
+        cm.sbmtbtn.click();
+
+
+    }
+
 }
