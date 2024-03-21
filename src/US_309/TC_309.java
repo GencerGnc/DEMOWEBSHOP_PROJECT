55fd8d348dafc155562b1adb5ba3fd87cf975d58
Gencer <gencer-genc@outlook.com>
2024-03-21 14:28:09 +0300

    309 solved

--- src/US_309/TC_309.java
+++ src/US_309/TC_309.java
@@ -1,54 +1,81 @@
 package US_309;
 
-import US_303.TC303_304_ELEMENTS;
 import Utility.BaseDriver;
 import Utility.Tools;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Pdf;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+
 public class TC_309 extends BaseDriver {
+
     @Test
-    public void Tc_304() {
-        TC303_304_ELEMENTS elements1 = new TC303_304_ELEMENTS();
-        UrlOpen();
+    public void Test() throws AWTException, IOException, UnsupportedFlavorException {
+        TC_309_Element elements=new TC_309_Element();
 
-        elements1.addcart.click();
+        driver.get("https://www.e-junkie.com/wiki/demo/paypal");
 
+
+
+        elements.addcart.click();
+        driver.switchTo().frame(elements.iframe1);
+        elements.CreditCard.click();
+
+        elements.email.sendKeys("Gencer@gmail.com");
+        elements.confirmEmail.sendKeys("Gencer@gmail.com");
+        elements.NameCard.sendKeys("Gencer");
+        elements.adress1.sendKeys("ankara");
+        elements.adress2.sendKeys("ankara");
+        elements.city.sendKeys("ankara");
+        elements.Zip.sendKeys("010101");
+
+
+
+        elements.inputCard.sendKeys("4242 4242 4242 4242");
+        elements.date.sendKeys("1224");
+        elements.ccvInput.sendKeys("000");
+        elements.payButton.click();
+
+        Tools.Bekle(10);
+
+        elements.download.click();
         Tools.Bekle(3);
-        driver.switchTo().frame(elements1.iframeodeme);
-        Tools.Bekle(1);
-        elements1.creditCard.click();
-        Tools.Bekle(3);
+        String filePath = "C:/Users/Gencer/Downloads/demo.pdf";
 
+        try {
+            Desktop.getDesktop().open(new File(filePath));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        elements1.mail.sendKeys("gencer@gmail.com");
-        elements1.confirmail.sendKeys("gencer@gmail.com");
-        elements1.nameOncard.sendKeys("gencer genc");
+        // Checking the content of the PDF file
+        PDDocument document = PDDocument.load(new File(filePath));
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+        String pdfText = pdfStripper.getText(document);
 
+        String searchText = "Gencer";
 
-        driver.switchTo().frame(2);
-
-        elements1.credtCardInput.sendKeys("4242 4242 4242 4242");
-        elements1.experidationDay.sendKeys("1224");
-        elements1.ccv.sendKeys("000");
-
-        driver.switchTo().parentFrame();
-
-        elements1.payclick.click();
-
-        wait.until(ExpectedConditions.urlContains("gencer"));
-
-
-        elements1.download.click();
-
-        Assert.assertTrue(elements1.download.isEnabled());
-
-
-        if (elements1.download.isEnabled()) {
-            System.out.println("TEST PASSED");
-        } else System.out.println("TEST FAIL");
+        if (pdfText.contains(searchText)) {
+            System.out.println("The searched text in the PDF was found..");
+        } else {
+            System.out.println("The searched text in the PDF was not found..");
+        }
 
 
     }
+
+
 }
